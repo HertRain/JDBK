2aff0a20c375c66ecf1da43b320bd0404a2a2380
Hert <1371116865@qq.com>
2017-06-29 21:40:28 +0800

    完成数据抓取

--- app/src/main/java/com/example/administrator/jdbk/MainActivity.java
+++ app/src/main/java/com/example/administrator/jdbk/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.administrator.jdbk;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.example.administrator.jdbk.Utils.OkHttpUtils;
+import com.example.administrator.jdbk.bean.Subject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +18,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Randomtest(View view) {
+        OkHttpUtils<Subject> utils=new OkHttpUtils<>(getApplicationContext());
+        String uri="http://101.251.196.90:8080/JztkServer/examInfo ";
+        utils .url(uri)
+                .targetClass(Subject .class )
+                .execute(new OkHttpUtils.OnCompleteListener <Subject>() {
+
+                    @Override
+                    public void onSuccess(Subject result) {
+                        Log.e("main","result"+result);
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e("main","error"+error);
+                    }
+                });
         Intent intent=new Intent(MainActivity.this,ExamActivity.class);
         startActivity(intent);
-        MainActivity .this.finish() ;
+
+    }
+
+    public void Exit(View view) {
+        finish() ;
     }
 }
