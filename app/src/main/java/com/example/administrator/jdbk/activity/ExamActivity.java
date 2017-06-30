692a0fd0c7860b901c6b4df0a6730db0a156bec6
HertRain <1371116865@qq.com>
2017-06-30 10:36:32 +0800

    把抓取到的数据显示在TextView上

--- app/src/main/java/com/example/administrator/jdbk/activity/ExamActivity.java
+++ app/src/main/java/com/example/administrator/jdbk/activity/ExamActivity.java
@@ -3,17 +3,39 @@ package com.example.administrator.jdbk.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.example.administrator.jdbk.Examapplication;
 import com.example.administrator.jdbk.R;
+import com.example.administrator.jdbk.bean.ExamInfo;
 
 /**
  * Created by Admin on 2017/6/29.
  */
 
 public class ExamActivity extends AppCompatActivity {
+    TextView textView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
          super.onCreate(savedInstanceState);
         setContentView(R.layout .activity_randomtest);
+        initView();
+        initData();
+    }
+
+    private void initView() {
+      textView =(TextView) findViewById(R.id.tv_examinfo);
+    }
+
+    private void initData() {
+       ExamInfo examinfo=Examapplication.getInstance().getExamInfo() ;
+        if(examinfo !=null)
+        {
+            showData(examinfo);
+        }
+    }
+
+    private void showData(ExamInfo examinfo) {
+        textView .setText(examinfo .toString()) ;
     }
 }
