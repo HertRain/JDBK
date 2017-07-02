d55e4bc969afe92812931b95545ea88256f2b6fb
Hert <1371116865@qq.com>
2017-07-02 10:09:13 +0800

    抓取试题

--- app/src/main/java/com/example/administrator/jdbk/Examapplication.java
+++ app/src/main/java/com/example/administrator/jdbk/Examapplication.java
@@ -1,11 +1,15 @@
 package com.example.administrator.jdbk;
 
 import android.app.Application;
+import android.database.CursorJoiner;
+import android.graphics.Interpolator;
 import android.util.Log;
 
 import com.example.administrator.jdbk.Utils.OkHttpUtils;
+import com.example.administrator.jdbk.Utils.ResultUtils;
 import com.example.administrator.jdbk.bean.ExamInfo;
-import com.example.administrator.jdbk.bean.Question;
+import com.example.administrator.jdbk.bean.Exam;
+import com.example.administrator.jdbk.bean.Result;
 
 import java.util.List;
 
@@ -15,7 +19,7 @@ import java.util.List;
 
 public class Examapplication extends Application {
     ExamInfo examInfo ;
-    List <Question> mQuestion;
+    List <Exam> mQuestion;
     private static Examapplication instance;
     @Override
     public void onCreate() {
@@ -25,30 +29,60 @@ public class Examapplication extends Application {
     }
 
     private void initData() {
-        OkHttpUtils<ExamInfo> utils=new OkHttpUtils<>(getApplicationContext());
-        String uri="http://101.251.196.90:8080/JztkServer/examInfo ";
-        utils .url(uri)
-                .targetClass(ExamInfo.class )
-                .execute(new OkHttpUtils.OnCompleteListener <ExamInfo>() {
+        //创建一个一个线程来加载数据
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpUtils<ExamInfo> utils=new OkHttpUtils<>(getApplicationContext());
+                String uri="http://101.251.196.90:8080/JztkServer/examInfo ";
+                utils .url(uri)
+                        .targetClass(ExamInfo.class )
+                        .execute(new OkHttpUtils.OnCompleteListener <ExamInfo>() {
 
-                    @Override
-                    public void onSuccess(ExamInfo result) {
-                        Log.e("main","result"+result);
-                        examInfo =result ;
-                    }
+                            @Override
+                            public void onSuccess(ExamInfo result) {
+                                Log.e("main","result"+result);
+                                examInfo =result ;
+                            }
 
-                    @Override
-                    public void onError(String error) {
-                        Log.e("main","error"+error);
-                    }
-                });
+                            @Override
+                            public void onError(String error) {
+                                Log.e("main","error"+error);
+                            }
+                        });
+                OkHttpUtils <String> utils1 =new OkHttpUtils<>(instance);
+                String url2="http://101.251.196.90:8080/JztkServer/getQuestions?testType=rand";
+                utils1 .url(url2)
+                        .targetClass(String.class)
+                        .execute(new OkHttpUtils.OnCompleteListener<String>() {
+
+                            @Override
+                            public void onSuccess(String jsonStr) {
+                                Result result=ResultUtils.getListResultFromJson(jsonStr);
+                                if(result!=null && result .getError_code() ==0)
+                                {
+                                    List<Exam> list =result .getResult() ;
+                                    if(list !=null && list.size()>0){
+                                        mQuestion =list;
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onError(String error) {
+                                Log.e("main","error"+error);
+                            }
+                        });
+            }
+        }).start();
+
     }
 
     public ExamInfo getExamInfo() {
         return examInfo;
     }
 
-    public List<Question> getmQuestion() {
+    public List<Exam> getmQuestion() {
         return mQuestion;
     }
 
--- app/src/main/java/com/example/administrator/jdbk/activity/ExamActivity.java
+++ app/src/main/java/com/example/administrator/jdbk/activity/ExamActivity.java
@@ -3,18 +3,23 @@ package com.example.administrator.jdbk.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.ListMenuItemView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.administrator.jdbk.Examapplication;
 import com.example.administrator.jdbk.R;
+import com.example.administrator.jdbk.bean.Exam;
 import com.example.administrator.jdbk.bean.ExamInfo;
+import java.util.List;
 
 /**
  * Created by Admin on 2017/6/29.
  */
 
 public class ExamActivity extends AppCompatActivity {
-    TextView textView;
+    TextView textView,tvExamTitle,tvop1,tvop2,tvop3,tvop4;
+    ImageView Image;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
          super.onCreate(savedInstanceState);
@@ -24,7 +29,14 @@ public class ExamActivity extends AppCompatActivity {
     }
 
     private void initView() {
-      textView =(TextView) findViewById(R.id.tv_examinfo);
+        textView =(TextView) findViewById(R.id.tv_examinfo);
+        tvExamTitle=(TextView) findViewById(R.id.tv_exam_title);
+        tvop1 =(TextView) findViewById(R.id.tv_exam_op1);
+        tvop2 =(TextView) findViewById(R.id.tv_exam_op2);
+        tvop3 =(TextView) findViewById(R.id.tv_exam_op3);
+        tvop4 =(TextView) findViewById(R.id.tv_exam_op4);
+        Image =(ImageView) findViewById(R.id.Iv_exam_image);
+
     }
 
     private void initData() {
@@ -32,6 +44,22 @@ public class ExamActivity extends AppCompatActivity {
         if(examinfo !=null)
         {
             showData(examinfo);
+        }
+        List<Exam> list=Examapplication .getInstance().getmQuestion() ;
+        if(list!=null)
+        {
+            showExam(list);
+        }
+    }
+
+    private void showExam(List<Exam> list) {
+        Exam exam=list.get(0);
+        if(exam!=null){
+            tvExamTitle.setText(exam .getQuestion() ) ;
+            tvop1 .setText(exam.getItem1()) ;
+            tvop2 .setText(exam.getItem2()) ;
+            tvop3 .setText(exam.getItem3()) ;
+            tvop4 .setText(exam.getItem4()) ;
         }
     }
 
